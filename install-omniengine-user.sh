847705a335996c52d9fb56d352fa1561cd687f05
Sean Gilligan <sean@msgilligan.com>
2014-10-09 09:46:51 -0700

    Add OmniEngine to Cron as part of install

--- install-omniengine-user.sh
+++ install-omniengine-user.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-REPOURL="https://github.com/mastercoin-MSC/omniEngine.git"
-BRANCH="master"
+#REPOURL="https://github.com/mastercoin-MSC/omniEngine.git"
+#BRANCH="master"
+REPOURL="https://github.com/msgilligan/omniEngine.git"
+BRANCH="msgilligan-vagrant"
 REPODIR="omniEngine"
 
 # Clone and checkout using passed (usually from Vagrant) parameters
@@ -8,12 +10,14 @@ git clone --no-checkout $REPOURL $REPODIR
 cd $REPODIR
 git checkout $BRANCH
 
+mkdir -p logs
+
 mkdir -p ~/.omni
 mkdir -p ~/.bitcoin
 
 cp /vagrant/bitcoin.conf ~/.bitcoin/bitcoin.conf
 cp /vagrant/sql.conf ~/.omni
 
-
+python install/installOmniEngineCronJob.py
 
 
