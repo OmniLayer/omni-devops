b6c482ba4d57e3c6ef23976acfdfaba26c5f6c36
Sean Gilligan <sean@msgilligan.com>
2014-11-02 16:43:24 -0800

    Disable install of SX

--- install-omniwallet-root.sh
+++ install-omniwallet-root.sh
@@ -3,7 +3,8 @@ MSC_USER=$(logname) #possibly use this versus passed in args
 MSC_GROUP=$2
 echo "Running install-sx..."
 cd /vagrant/res
-bash install-sx.sh
+#Disable installing SX
+#bash install-sx.sh
 
 mkdir -p /var/lib/omniwallet
 chown -R $MSC_USER:$MSC_GROUP /var/lib/omniwallet
